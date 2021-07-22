e99ac66e643bf8618bd4863d4bb4cbda4093853e
Three-team-org <three.teamorg@gmail.com>
2021-07-22 02:34:57 -0700

    update for messages

--- app/src/main/java/io/github/subhamtyagi/ocr/MainActivity.java
+++ app/src/main/java/io/github/subhamtyagi/ocr/MainActivity.java
@@ -566,7 +566,7 @@ public class MainActivity extends AppCompatActivity implements TessBaseAPI.Progr
             String clean_text = Html.fromHtml(text).toString().trim();
             Log.d(TAG, "onPostExecute: text\n" + clean_text);
             showOCRResult(clean_text);
-            Toast.makeText(MainActivity.this, "With Confidence:" + mImageTextReader.getAccuracy() + "%", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "Identified Successfully!", Toast.LENGTH_SHORT).show();
 
             Utils.putLastUsedText(clean_text);
             Bitmap bitmap = loadBitmapFromStorage();
